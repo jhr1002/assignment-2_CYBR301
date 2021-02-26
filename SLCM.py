9f497185625aa19a2d218ea98c74d362c6eb72c6
jhr1002 <78226664+jhr1002@users.noreply.github.com>
2021-02-26 14:46:44 -0500

    typos

--- SLCM.py
+++ SLCM.py
@@ -1,6 +1,7 @@
 #Anthony Colon Jake Rowe
 
 def getusername_paswd():
+    validboth = False
     #We need to use inputs for user and password
     username=input("Username:")
     password=input("Password:")  #We can write the rules here if needed but not in directions
